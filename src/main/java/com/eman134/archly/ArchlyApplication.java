b5dcad35987d1d632b985a4029fdf003ff38c321
Eman134 <kaykeeman@gmail.com>
2025-01-04 20:18:02 -0300

    Starting project with spring

---
+++ src/main/java/com/eman134/archly/ArchlyApplication.java
@@ -0,0 +1,13 @@
+package com.eman134.archly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eman134/archly/ArchlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eman134.archly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
