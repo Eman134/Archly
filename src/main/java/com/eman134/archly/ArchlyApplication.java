8375eb80e2f81f9d2dc4809417f31ce9b3982c32
Eman134 <kaykeeman@gmail.com>
2025-01-05 00:35:11 -0300

    First tests with spring boot

--- src/main/java/com/eman134/archly/ArchlyApplication.java
+++ src/main/java/com/eman134/archly/ArchlyApplication.java
@@ -2,12 +2,29 @@ package com.eman134.archly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ArchlyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArchlyApplication.class, args);
+	}
+
+	@GetMapping("/question")
+	public ResponseEntity<String> question(@RequestParam String question) {
+		String answer = "resposta aleatória";
+		return ResponseEntity.ok(answer);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "Hello guy!";
 	}
 
 }
